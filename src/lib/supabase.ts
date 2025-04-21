f3d0ba965b7eb593a7b85dd56ec77c8a123cc29e
José Henrick <seuemail@exemplo.com>
2025-04-21 17:33:19 -0300

    feat: adiciona buscarClientePorNome ao serviço de clientes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mbdsxzcmxdhvbazpfxcs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZHN4emNteGRodmJhenBmeGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjM4NzIsImV4cCI6MjA2MDgzOTg3Mn0.SYp6AV07CTZtvkYz6P6Yue8fvAFHzWT95JR1RnYlxyM';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
+);
